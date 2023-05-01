8011d284fed54a4be7f3f606db8ac1771f2ebb03
DAniel Martin <damarpele@gmail.com>
2023-05-01 17:32:33 +0200

    Pagina registro iniciada

--- booking_webapp/src/main/java/com/booking/controller/IniciarSesionController.java
+++ booking_webapp/src/main/java/com/booking/controller/IniciarSesionController.java
@@ -4,6 +4,7 @@ package com.booking.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -38,6 +39,10 @@ public class IniciarSesionController{
 		}
 	}
 	
+	@GetMapping(value = "/registrarse")
+	public String moverseRegistrarse() {
+		return "registrarse";
+	}
 	
 
 }
